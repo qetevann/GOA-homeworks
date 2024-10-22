b782cb672a3428a1c4b18b4f2206a4b1451714ed
qetevann <queenling240@gmail.com>
2024-10-22 23:46:14 +0400

    first commit

---
+++ day 01/homework/homework.py
@@ -0,0 +1,16 @@
+name = "qetevan"
+surname = "makharashvili"
+
+#print(name)
+#The print function is passed an object to be displayed on the screen
+
+
+name = "qetevan"
+surname = "makharashvili"
+age = 16
+height = 1.75
+
+loves_freedom = True 
+likes_geography = False 
+
+print(name + " " + surname + " " + str(age) + " " + str(height))
