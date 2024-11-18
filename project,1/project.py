217ffde9455bd678a21ed37fbed6b2b81d10c015
qetevann <queenling240@gmail.com>
2024-11-18 21:29:50 +0400

    second commit

---
+++ project,1/project.py
@@ -0,0 +1,50 @@
+
+name = (input(" Enter your name: "))
+age = (input(" Enter your age: "))
+pasw = (input(" Enter your password: "))
+
+
+print(" Welcome, " + name + "!")
+
+ans = (input("  "))
+if ans == "how to calculate percentage":
+    print("To calculate percentages divide the numerator by the denominator and multiply by 100 this will give you the fraction as a percentage.")
+elif ans == "how to calculate iq":
+    print("To calculate a person's IQ, take the person's mental age and divide it by their chronological age. Next, divide by 100.")
+elif ans == "the most read books":
+    print("The most read books: Harry Potter Series by J.K. Rowling, The Lord of the Rings by J.R.R. Tolkien.")
+else:
+    print("I don't understand the question")
+
+
+ans2 = (input("  "))
+if ans2 == "the most famous people":
+    print("Worlds most famous people: Walt Disney, Michael Jackson, Johnny Depp.")
+elif ans2 == "who's the richest person in the world":
+    print("Elon Musk, CEO of Tesla, is the richest person and the richest man in the world with a net worth of $263 billion.")
+elif ans2 == "how many languages are in the world":
+    print("It's estimated that up to 7,000 different languages are spoken around the world.")
+#else:
+ #   print("unknown")
+
+
+ans3 = (input("  "))
+if ans3 == "what is github":
+    print("GitHub is a cloud-based platform where you can store, share, and work together with others to write code.")
+elif ans3 == "is starry night impressionism":
+    print("The Starry Night is Post-Impressionist art. It follows the Post-Impressionist style, with its focus on nature depicted through color and light.")
+elif ans3 == "what are sharps in music":
+    print("A sharp raises a note by a semitone; a flat lowers it by a semitone; a natural restores it to the original pitch.")
+else:
+   print("I don't understand the question")
+
+
+ans4 = (input("  "))
+if ans4 == "the hardest language in the world":
+    print("Hardest language in the world: Mandarin Unanimously speaking, Mandarin Chinese is considered to be the hardest language in the world.")
+elif ans4 == "who actually invented internet":
+    print("British computer scientist Tim Berners-Lee invented the World Wide Web, a key part of the Internet used today.")
+elif ans4 == "how does wifi work":
+    print("Wi-Fi uses radio waves to transmit data from your wireless router to your Wi-Fi enabled devices like your TV, smartphone, tablet and computer.")
+else:
+    print("I don't understand the question")
