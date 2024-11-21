92fa5392d18480c35510c2b1a4160453d5bb7b61
qetevann <queenling240@gmail.com>
2024-11-21 18:37:17 +0400

    first commit

---
+++ day 14/classwork.py
@@ -0,0 +1,19 @@
+# !=  -  არ უდრის
+
+
+# წინ თუ უწერია !, უარყოფას ნიშნავს
+
+age = 11
+
+print(age > 18 and age < 30)
+
+
+
+
+
+user = (input(" Enter your name: "))
+
+print(user == "qetevan" or user == "qeta" or user == "qeti")
+
+
+
