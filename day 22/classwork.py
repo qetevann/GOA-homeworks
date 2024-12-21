d6f1ebda388756cd41f25327797b7be8a2e9e6c1
qetevann <queenling240@gmail.com>
2024-12-22 02:44:49 +0400

    first commit

---
+++ day 22/classwork.py
@@ -0,0 +1,35 @@
+sum = int(input(""))
+
+for i in range(1, sum):
+
+    sum = sum + i
+
+print(sum)
+
+
+
+c = int(input(""))
+
+even = 0
+odd = 0
+
+for i in range(1, c):     
+    if i % 2 == 0:
+        even = even + i
+    else:
+       odd = odd + i
+    
+
+
+
+q = int(input(""))
+sum = 0
+
+i = 0
+
+while i < 50:
+    sum = sum + i
+
+    i = i + 1
+print(sum)
+                        
