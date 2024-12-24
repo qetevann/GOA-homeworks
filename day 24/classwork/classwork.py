a8cbc37398c3ed265d5ef75ea5cbc6d9e08ec937
qetevann <queenling240@gmail.com>
2024-12-25 00:35:10 +0400

    first commit

---
+++ day 24/classwork/classwork.py
@@ -0,0 +1,37 @@
+# level 24:
+# 1) გააკეთეთ სარეგისტრაციო, მომხმარებელს შემოატანიეთ სახელი და პაროლი, 
+# რეგისტრაციის შემდეგ მოხმარებელი უნდა შევიდეს ექაუნთზე, შეეკითხეთ სახელი და პაროლი 
+# მომხარებელს რათა შევიდეს ექაუნთზე სანამ მომხმარებელი არ შეიტანს იმ ინფორმაციას 
+# რაც შეიყვანა რეგისტრაციის დროს მანამ დაიბეჭდოს რომ შეტანილი ინფორმაცია არასწორია და შეეკითხოს თავიდან, 
+# ხოლო თუ მომხმარებელმა ყველაფერი სწორად შეიყვანა დაიბეჭდოს welcome
+
+# name = input(" Enter your name: ")
+
+# passw = input(" Enter password: ")
+
+# while passw != "Qetevan24":
+#     print("Wrong password")
+#     passw = input(" Enter correct password: ")
+
+
+# print(" welcome ")
+
+
+# name = input(" enter your name: ")
+
+# password = input(" enter your password: ")
+
+# if name != "qetevan" or password != "2403":
+#     print(" wrong password ")
+# else:
+#     print(" welcome ")
+
+
+
+
+
+
+
+# my_list = ["qetevan", "nini"]
+
+# print(list)
