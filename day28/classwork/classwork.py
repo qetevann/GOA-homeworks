bca7cd537a0a75a121f602c55ed3db68b1cfe9e3
qetevann <queenling240@gmail.com>
2025-01-12 23:49:49 +0400

    first commit

---
+++ day28/classwork/classwork.py
@@ -0,0 +1,22 @@
+# 1)შექმენით სია, რომელიც შეიცავს 6 სხვადასხვა ცხოველის სახელს. 
+# წაშალეთ მეორე და მეოთხე ცხოველის სახელი 
+# და გამოიტანეთ განახლებული სია.
+
+
+animals = ["dog", "cat", "snake", "lion", "zebra", "tiger"]
+
+
+animals.remove("snake")
+
+animals.remove("zebra")
+
+
+print(animals)
+
+
+
+fruit = ["apple", "pear", "grape", "watermelon", "strawberry"]
+
+fruit[3] = "pineapple"
+
+print(fruit)
